95804c1a85d08a5b762f8ae1b69fd0a2d95321f4
quinkinser <quinkinser@gmail.com>
2018-08-04 17:34:37 -0700

    Add a component that uses typescript

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import HelloWorld from './components/HelloWorld';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -21,8 +23,9 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome ddd React Native! djdjdj</Text>
-        <Text style={styles.instructions}>To get stdddarted, edit App.tsx</Text>
+        <HelloWorld name="Joe Johnson" enthusiasmLevel={5} />
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
